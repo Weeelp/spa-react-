84e3dd11e9ddce6f34c98250d096b27f11410ae0
Elizaveta <e.nesterovich007@gmail.com>
2025-01-05 19:19:11 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import "./styles/App.css";
+import PostList from "./components/PostList";
+import PostForm from "./components/PostForm";
+import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/MyModal/MyModal";
+import MyButton from "./components/UI/button/MyButton";
+import Loader from "./components/UI/Loader/Loader";
+import { usePosts } from "./hooks/usePost";
+import { useFetching } from "./hooks/useFetching";
+import PostService from "./API/PostService";
+
+function App() {
+  const [posts, setPosts] = useState([]);
+  const [filter, setFilter] = useState({ sort: "", query: "" });
+  const [modal, setModal] = useState(false);
+  const sortedAndSerachedPosts = usePosts(posts, filter.sort, filter.query);
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const posts = await PostService.getAll();
+    setPosts(posts);
+  });
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
+    setModal(false);
+  };
+
+  const removePost = (post) => {
+    setPosts(posts.filter((p) => p.id !== post.id));
+  };
+
+  return (
+    <div className="App">
+      <MyButton style={{ marginTop: "30px" }} onClick={() => setModal(true)}>
+        Создать пользователя
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost} />
+      </MyModal>
+
+      <hr style={{ margin: "15px 0" }} />
+      <PostFilter filter={filter} setFilter={setFilter} />
+
+      {postError && <h1>Произошла ошибка: {postError}</h1>}
+
+      {isPostsLoading ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "50px",
+          }}
+        >
+          <Loader />
+        </div>
+      ) : (
+        <PostList
+          remove={removePost}
+          posts={sortedAndSerachedPosts}
+          title="1 Список постов 1"
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PostList.jsx
@@ -0,0 +1,38 @@
+import React, { createRef } from "react";
+import PostItem from "./PostItem";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+
+const PostList = ({ posts, title, remove }) => {
+  if (!posts.length) {
+    return <h1 style={{ textAlign: "center" }}>Посты не найдены!</h1>;
+  }
+
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>{title}</h1>
+      <TransitionGroup>
+        {posts.map((post, index) => {
+          const nodeRef = createRef(); // Создание ref для CSSTransition
+          return (
+            <CSSTransition
+              key={post.id}
+              timeout={500}
+              classNames="post"
+              nodeRef={nodeRef} // Указываем nodeRef
+            >
+              <div ref={nodeRef}>
+                <PostItem
+                  remove={remove}
+                  number={index + 1}
+                  post={post}
+                ></PostItem>
+              </div>
+            </CSSTransition>
+          );
+        })}
+      </TransitionGroup>
+    </div>
+  );
+};
+
+export default PostList;
